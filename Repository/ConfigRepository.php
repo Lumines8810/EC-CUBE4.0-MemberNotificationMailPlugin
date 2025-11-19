68d64bf67c7cc58b4d5879a10796366ecd4727f9
Hayato Yokoyama <h.yokoyama@lumines.co.jp>
2025-11-19 15:40:24 +0900

    Add ManagerRegistry alias for newer Doctrine versions

--- Repository/ConfigRepository.php
+++ Repository/ConfigRepository.php
@@ -6,6 +6,15 @@ use Doctrine\Common\Persistence\ManagerRegistry;
 use Eccube\Repository\AbstractRepository;
 use Plugin\CustomerChangeNotify\Entity\Config;
 
+// Doctrine ORM 2.10+ moves ManagerRegistry to Doctrine\Persistence. EC-CUBE 4.0.3
+// still ships Doctrine\Common\Persistence but a composer update can drop it,
+// causing a fatal "Class 'Doctrine\\Common\\Persistence\\ManagerRegistry' not
+// found" error when the repository is autoloaded. Provide a runtime alias to the
+// new namespace so both dependency stacks work.
+if (!interface_exists(ManagerRegistry::class) && interface_exists(\Doctrine\Persistence\ManagerRegistry::class)) {
+    class_alias(\Doctrine\Persistence\ManagerRegistry::class, ManagerRegistry::class);
+}
+
 /**
  * ConfigRepository.
  */
